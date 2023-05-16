69959982debd6a8bfed18ba2f864d0bfa9845a2f
MustafaIrshaidd <mustafa-irshaid_2001@hotmail.com>
2023-05-16 23:31:33 +0300

    basic layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,19 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.font = "18px Arial";
-ctx.strokeText("This Canvas Dear Aamer", 5, 60);
+ctx.strokeText("This is Canvas Dear Aamer", 5, 60);
 
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.beginPath();
-ctx.arc(181, 55, 30, 0, 2 * Math.PI);
+ctx.arc(200, 55, 30, 0, 2 * Math.PI);
 ctx.stroke();
+
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "18px Arial";
+ctx.strokeText("الف مبروك الفوز للملوخية", 130, 110);
+
+
